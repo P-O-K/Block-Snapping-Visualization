d35e377c07bb10159d9c0de0ed23c99a485861ef
Patrick O'Keefe <67643795+P-O-K@users.noreply.github.com>
2021-09-01 18:24:55 +0100

    Add files via upload

---
+++ Constants.py
@@ -0,0 +1,21 @@
+
+
+
+DISPLAY_WINDOW_NAME         :str = 'BlockSnapping'
+DISPLAY_WIDT                :int = 500
+DISPLAY_HEIGHT              :int = 500
+
+FRAMERATE                   :int = 60
+
+BLOCK_SCALE_FACTOR          :float = 3
+BLOCK_WIDTH                 :int = 30
+BLOCK_HEIGHT                :int = 30
+BLOCK_FILL                  :int = 0
+BLOCK_LINEWIDTH             :int = 1
+
+BLOCK_COLOR                 :tuple = ( 255, 255, 255 )
+BOUNDING_BOX_COLOR          :tuple = (  50, 150, 200 )
+SNAPPING_POINT_COLOR        :tuple = (  30, 150,  80 )
+
+BACKGROUND_COLOR            :tuple = (  30,   7,  12 )
+
